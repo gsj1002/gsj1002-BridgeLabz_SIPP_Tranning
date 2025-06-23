2644f6698548addf6e34f5f09bc37e8ed35da8ae
govind singh jadon <156882460+gsj1002@users.noreply.github.com>
2025-06-23 12:41:04 +0530

    Delete practics_problem directory

--- practics_problem/add.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class add{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		System.out.print(n+m);
-	}
-}
